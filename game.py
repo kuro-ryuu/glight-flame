03773b35dbd96e27c7cc362f160e9cad54b2bedb
BlindMyEyes <nhatquang14022007@gmail.com>
2025-10-28 14:50:17 +0700

    testing

--- game.py
+++ game.py
@@ -1,9 +1,24 @@
 import connect_to_db
 import random
 import time
-db_connection = connect_to_db().connect()
+import mysql.connector
+
+db_connection = connect_to_db.connect()
 def map_choosing(): 
     pass
+def get_airports(conti):
+    sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
+FROM airport
+WHERE continent = '{conti}' 
+AND type='large_airport'
+ORDER by RAND()
+LIMIT 30;"""
+    cursor = db_connection.cursor(dictionary=True)
+    cursor.execute(sql)
+    result = cursor.fetchall()
+    return result
+choose_continent = input("Enter your continent (EU or AS): ")
+print(get_airports(choose_continent))
 def obstacle_gen(number):
     pass
 def disaster_gen(delay,magnitude):
