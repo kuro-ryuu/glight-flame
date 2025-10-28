8b7c9be17190b2d814be432f09089fb7fec64d4f
BlindMyEyes <nhatquang14022007@gmail.com>
2025-10-28 15:44:40 +0700

    testing

--- game.py
+++ game.py
@@ -7,7 +7,7 @@ db_connection = connect_to_db.connect()
 def map_choosing(): 
     pass
 def get_airports(conti):
-    sql = """SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
+    sql = f"""SELECT iso_country, ident, name, type, latitude_deg, longitude_deg
 FROM airport
 WHERE continent = '{conti}' 
 AND type='large_airport'
@@ -17,8 +17,24 @@ LIMIT 30;"""
     cursor.execute(sql)
     result = cursor.fetchall()
     return result
+
 choose_continent = input("Enter your continent (EU or AS): ")
-print(get_airports(choose_continent))
+airports_in_continent = get_airports(choose_continent)
+final_result = [list(i) for i in airports_in_continent]
+airport_list = final_result[2]
+
+for airport in airports_in_continent:
+    print(airport["name"])
+
+airport_pick = ""
+for i in range(len(airports_in_continent)):
+    airports_in_continent[i] = airports_in_continent[i]['name'].lower()
+while airport_pick not in airports_in_continent:
+    airport_pick = input("Select your airport: ").lower()
+    if airport_pick in airports_in_continent:
+        print("Airports selected")
+    else:
+        print("Airports not selected, please try again")
 def obstacle_gen(number):
     pass
 def disaster_gen(delay,magnitude):
